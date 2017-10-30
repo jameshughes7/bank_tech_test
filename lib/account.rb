e2d8cf3c651e1b564b30deb0b5caaa03af241c65
James Hughes <hugjimbo77@gmail.com>
2017-10-30 12:46:21 +0000

    added credit and debit functionality

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,20 @@
 class Account
+
   def initialize
     @balance = 0
   end
 
   def show_balance
+    @balance
+  end
+
+  def credit(amount)
+    @balance += amount
+    @balance
+  end
+
+  def debit(amount)
+    @balance -= amount
     @balance
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,13 +1,29 @@
 require 'account'
 
-  describe Account do
-    it 'should create a new instance of Account class' do
-      account = Account.new
-      expect(account).to be_instance_of(Account)
-    end
+describe Account do
+  subject(:account) { described_class.new(0) }
 
-    it 'should initialize with a default zero balance' do
-      account = Account.new
-      expect(account.show_oobalance).to eq(0)
-    end
+  it 'should create a new instance of Account class' do
+    account = Account.new
+    expect(account).to be_instance_of(Account)
   end
+
+  it 'should initialize with a default zero balance' do
+    account = Account.new
+    expect(account.show_balance).to eq(0)
+  end
+
+  it 'should be able to receive a credit' do
+    account = Account.new
+    expect(account).to respond_to(:credit).with(1).argument
+    account.credit(100)
+    expect(account.show_balance).to eq(100)
+  end
+
+  it 'should be able to receive a debit' do
+    account = Account.new
+    expect(account).to respond_to(:debit).with(1).argument
+    account.debit(100)
+    expect(account.show_balance).to eq(-100)
+  end
+end
