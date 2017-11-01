dc0b65af5f435939b2833b986c8e99037ef3f018
James Hughes <hugjimbo77@gmail.com>
2017-11-01 10:52:19 +0000

    added desposit functionality to transaction class

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,14 @@
+require_relative 'account'
+
 class Transaction
 
   def initialize
     @transactions = []
   end
 
-  
+  def deposit(amount,account)
+    account.credit(amount)
+  end
+
+
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,8 +1,18 @@
 require 'transaction'
 
 describe Transaction do
+  subject(:transaction) { described_class.new }
+
   it 'should create a new instance of Transaction class' do
     transaction = Transaction.new
     expect(transaction).to be_instance_of(Transaction)
   end
+
+  it 'should make a deposit' do
+    transaction = Transaction.new
+    expect(transaction).to respond_to(:deposit).with(2).arguments
+    account = Account.new
+    expect{ transaction.deposit(100, account) }.to change { account.show_balance }.by(100)
+  end
+
 end
