fe0a43a97ec2cebdd024cac979ddcfbacac226e7
James Hughes <hugjimbo77@gmail.com>
2017-11-02 09:51:43 +0000

    added receive_transaction records to desposit and withdrawal methods in Transaction class

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -21,4 +21,20 @@ describe Transaction do
     account = Account.new
     expect{ transaction.withdrawal(100, account) }.to change { account.show_balance }.by(-100)
   end
+
+  it 'should receive debit transaction record' do
+    transaction = Transaction.new
+    account = Account.new
+    account.receive_transactions(nil, 100)
+    expect(account.transactions[0]).to eq(account.transaction_record)
+    expect(account.transactions[0]).to eq({ 'date' => Time.now.strftime("%d/%m/%y"), 'credit' => nil, 'debit' => 100, 'balance' => 0 })
+  end
+
+  it 'should receive credit transaction record' do
+    transaction = Transaction.new
+    account = Account.new
+    account.receive_transactions(100, nil)
+    expect(account.transactions[0]).to eq(account.transaction_record)
+    expect(account.transactions[0]).to eq({ 'date' => Time.now.strftime("%d/%m/%y"), 'credit' => 100, 'debit' => nil, 'balance' => 0 })
+  end
 end
